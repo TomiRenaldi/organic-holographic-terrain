1d675cb3ca9c01e89a5c49e0c2fe032e6e817deb
TomiRenaldi <tomirenaldi2@gmail.com>
2022-08-23 21:11:09 +0700

    :sparkles: Add overlay parallx configuration

--- src/Experience/components/Overlay.js
+++ src/Experience/components/Overlay.js
@@ -11,6 +11,8 @@ export default class Overlay
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.debug = this.experience.debug
+        this.camera = this.experience.camera
+        this.renderer = this.experience.renderer
 
         // Debug
         if (this.debug)
@@ -21,6 +23,7 @@ export default class Overlay
         }
 
         this.setOverlay()
+        this.setView()
     }
 
     setOverlay()
@@ -31,9 +34,9 @@ export default class Overlay
         this.overlay.colorA.value = '#5c5c5c'
         this.overlay.colorA.instance = new THREE.Color(this.overlay.colorA.value)
 
-        this.overlay.colorB = {}
-        this.overlay.colorB.value = '#000fff'
-        this.overlay.colorB.instance = new THREE.Color(this.overlay.colorB.value)
+        // this.overlay.colorB = {}
+        // this.overlay.colorB.value = '#000fff'
+        // this.overlay.colorB.instance = new THREE.Color(this.overlay.colorB.value)
 
         if (this.debug)
         {
@@ -52,24 +55,24 @@ export default class Overlay
                 this.overlay.colorA.instance.set(this.overlay.colorA.value)    
             })
 
-            debugFolder.addInput(
-                this.overlay.colorB,
-                'value',
-                {
-                    view: 'color'
-                }
-            )
-            .on('change', () => {
-                this.overlay.colorB.instance.set(this.overlay.colorB.value)    
-            })
+            // debugFolder.addInput(
+            //     this.overlay.colorB,
+            //     'value',
+            //     {
+            //         view: 'color'
+            //     }
+            // )
+            // .on('change', () => {
+            //     this.overlay.colorB.instance.set(this.overlay.colorB.value)    
+            // })
         }
 
         this.overlay.uniforms = {
             uSchemaColor: { value: this.overlay.colorA.instance },
-            uSchemaMultiplier: { value: 1.16 },
-            uSchemaOffset: { value: - 1 },
-            uOverlayColor: { value: this.overlay.colorB.instance },
-            uOverlayAlpha: { value: 1 }
+            uSchemaMultiplier: { value: 0.815 },
+            uSchemaOffset: { value: - 0.043 },
+            // uOverlayColor: { value: this.overlay.colorB.instance },
+            // uOverlayAlpha: { value: 1 }
         }
 
         if (this.debug)
@@ -90,11 +93,11 @@ export default class Overlay
                 { label: 'uSchemaOffset', min: -2, max: 2, step: 0.001 }
             )
 
-            debugFolder.addInput(
-                this.overlay.uniforms.uOverlayAlpha,
-                'value',
-                { label: 'uOverlayAlpha', min: 0, max: 1, step: 0.001 }
-            )
+            // debugFolder.addInput(
+            //     this.overlay.uniforms.uOverlayAlpha,
+            //     'value',
+            //     { label: 'uOverlayAlpha', min: 0, max: 1, step: 0.001 }
+            // )
         }
 
         this.overlay.geometry = new THREE.PlaneGeometry(2, 2) 
@@ -110,5 +113,76 @@ export default class Overlay
         this.overlay.mesh.userData.noBokeh = true
         this.overlay.mesh.frustumCulled = false
         this.scene.add(this.overlay.mesh)
+    }
+
+    setView()
+    {
+        this.view = {}
+        this.view.index = 0
+        this.view.settings = [
+            {
+                position: { x: 0, y: 2.124, z: - 0.172 },
+                rotation: { x: -1.489, y: - Math.PI, z: 0 },
+                focus: 2.14,
+                parallaxMultiplier: 0.25
+            },
+
+            {
+                position: { x: 1, y: 1.1, z: 0 },
+                rotation: { x: -0.833, y: 1.596, z: 1.651 },
+                focus: 1.1,
+                parallaxMultiplier: 0.12
+            },
+
+            {
+                position: { x: 1, y: 0.87, z: - 0.97 },
+                rotation: { x: - 0.638, y: 2.33, z: 0 },
+                focus: 1.36,
+                parallaxMultiplier: 0.12
+            },
+
+            {
+                position: { x: -1.43, y: 0.33, z: -0.144 },
+                rotation: { x: -0.312, y: -1.67, z: 0 },
+                focus: 1.25,
+                parallaxMultiplier: 0.12
+            }
+        ]
+
+        this.view.current = this.view.settings[this.view.index]
+
+        // Parallax
+        this.view.parallax = {}
+
+        this.view.parallax.target = {}
+        this.view.parallax.target.x = 0
+        this.view.parallax.target.y = 0
+
+        this.view.parallax.eased = {}
+        this.view.parallax.eased.x = 0
+        this.view.parallax.eased.y = 0
+        this.view.parallax.eased.multiplier = 4
+
+        // Apply
+        this.view.apply = () => 
+        {
+            // camera
+            this.camera.position.copy(this.view.current.position)
+            this.camera.rotation.x = this.view.current.rotation.x
+            this.camera.rotation.y = this.view.current.rotation.y
+
+            // Bokeh
+            this.renderer.postProcess.bokehPass.materialBokeh.uniforms.focus.value = this.current.focus
+
+            // Parallax
+            this.view.parallax.multiplier = this.view.current.parallaxMultiplier
+        }
+
+        // Change
+        this.view.change = (_index) => 
+        {
+            this.view = _index
+            this.view.current = this.view.settings[_index]
+        }
     }
 }
--- src/Experience/shaders/overlay/fragment.glsl
+++ src/Experience/shaders/overlay/fragment.glsl
@@ -1,8 +1,8 @@
 uniform vec3 uSchemaColor;
 uniform float uSchemaMultiplier;
 uniform float uSchemaOffset;
-uniform vec3 uOverlayColor;
-uniform float uOverlayAlpha;
+// uniform vec3 uOverlayColor;
+// uniform float uOverlayAlpha;
 
 varying vec2 vUv;
 
@@ -10,11 +10,13 @@ void main()
 {
     float distanceToCenter = smoothstep(0.0, 1.0, length(vUv - 0.5));
 
-    float schemaStrength = clamp(distanceToCenter * uSchemaMultiplier + uSchemaOffset, 0.0, 1.0);
+    // float schemaStrength = clamp(distanceToCenter * uSchemaMultiplier + uSchemaOffset, 0.0, 1.0);
 
-    vec3 colored = mix(uSchemaColor, uOverlayColor, (1.0 - schemaStrength) * uOverlayAlpha);
+    // vec3 colored = mix(uSchemaColor, uOverlayColor, (1.0 - schemaStrength) * uOverlayAlpha);
 
-    float alpha = schemaStrength + uOverlayAlpha;
+    // float alpha = schemaStrength + uOverlayAlpha;
 
-    gl_FragColor = vec4(colored, alpha);
+    float alpha = distanceToCenter * uSchemaMultiplier + uSchemaOffset;
+
+    gl_FragColor = vec4(uSchemaColor, alpha);
 }
